fa3ae679c8144c01f05d0bc80e6a68fab15da1d0
Rémi Michel <remi.michel38@gmail.com>
2025-02-10 11:39:38 +0100

    modal + datepicker

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -3,6 +3,12 @@ import { states } from "../../data";
 import { Select } from "select-p14-elodie";
 import { EmployeesContext } from "../../context";
 
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { fr } from 'date-fns/locale/fr';
+import Modal from "../modal/Modal";
+import { createPortal } from "react-dom";
+
 
 
 
@@ -10,6 +16,7 @@ const Form = () => {
   const {employees, setEmployees} = useContext(EmployeesContext)
   const [selectedState, setSelectedState] = useState(null)
   const [selectedDepartment, setSelectedDepartment] = useState(null)
+  const [showModal, setShowModal] = useState(false)
 // initialiser du state du formulaire avec des valeurs par défaut
   const [formData, setFormData] = useState({
     firstName: "",
@@ -40,7 +47,7 @@ const Form = () => {
   const newEmployeesData = employeesData.concat(formData)
   setEmployees(newEmployeesData)
   localStorage.setItem("employeesData", JSON.stringify(newEmployeesData));
-  console.log("Form submitted", formData);
+  setShowModal(true)
 };
   
  // fonction pour gérer la sélection d'un état dans la liste déroulante
@@ -52,17 +59,31 @@ const Form = () => {
     return (
       <>
         <form id="create-employee" onSubmit={handleSubmit}>
-        <label htmlFor="first-name">First Name</label>
+        <label htmlFor="firstName">First Name</label>
         <input type="text" id="firstName" value={formData.firstName} onChange={handleChange} />
 
-        <label htmlFor="last-name">Last Name</label>
+        <label htmlFor="lastName">Last Name</label>
         <input type="text" id="lastName" value={formData.lastName} onChange={handleChange} />
 
-        <label htmlFor="date-of-birth">Date of Birth</label>
-        <input type="date" id="dateOfBirth" value={formData.dateOfBirth} onChange={handleChange} />
+        <label htmlFor="dateOfBirth">Date of Birth</label>
+        <DatePicker
+          id="dateOfBirth"
+          selected={formData.dateOfBirth}
+          onSelect={(value) => handleChange({target: {id: 'dateOfBirth', value}})}
+          onChange={(value) => handleChange({target: {id: 'dateOfBirth', value}})}
+          locale={fr}
+          dateFormat={'dd/MM/yyyy'}          
+        />
 
-        <label htmlFor="start-date">Start Date</label>
-        <input type="date" id="startDate" value={formData.startDate} onChange={handleChange} />
+        <label htmlFor="startDate">Start Date</label>
+        <DatePicker
+          id="startDate" 
+          selected={formData.startDate}
+          onSelect={(value) => handleChange({target: {id: 'startDate', value}})}
+          onChange={(value) => handleChange({target: {id: 'startDate', value}})}
+          locale={fr}
+          dateFormat={'dd/MM/yyyy'}          
+        />
 
         <fieldset className="address">
           <legend>Address</legend>
@@ -99,6 +120,7 @@ const Form = () => {
           />
 
         <button type="submit">Save</button>
+        {(showModal) ? createPortal(<Modal handleCloseClick={() => setShowModal(false)}/>, document.body) : ''}
       </form>
       </>
     );
