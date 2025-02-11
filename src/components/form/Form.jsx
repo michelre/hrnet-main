c4171051a3d5593067e7ab5a67c5ad412ff6a4f4
ElodieAllegro <124352648+ElodieAllegro@users.noreply.github.com>
2025-02-11 17:56:47 +0100

    Form

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,45 +1,78 @@
+import { useState } from "react";
 const Form = () => {
+
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    dateOfBirth: "",
+    startDate: "",
+    street: "",
+    city: "",
+    state: "",
+    zipCode: "",
+    department: "",
+  });
+
+  // Gestion des changements des input
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setFormData({ ...formData, [id]: value });
+  }
+
+  // Gestion du submit du formulaire
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    localStorage.setItem("employeeData", JSON.stringify(formData));
+    console.log(formData);
+  }
+  
     return (
       <>
-        <form action="#" id="create-employee">
+        <form id="create-employee" onSubmit={handleSubmit}>
           <label htmlFor="first-name">First Name</label>
-          <input type="text" id="first-name" />
+          <input type="text" id="firstName" value={formData.firstName} onChange={handleChange} />
   
           <label htmlFor="last-name">Last Name</label>
-          <input type="text" id="last-name" />
+          <input type="text" id="last-name" value={formData.lastName} onChange={handleChange}/>
   
           <label htmlFor="date-of-birth">Date of Birth</label>
-          <input id="date-of-birth" type="text" />
+          <input id="date-of-birth" type="text" value={formData.dateOfBirth} onChange={handleChange}/>
   
           <label htmlFor="start-date">Start Date</label>
-          <input id="start-date" type="text" />
+          <input id="start-date" type="text" value={formData.startDate} onChange={handleChange}/>
   
           <fieldset className="address">
             <legend>Address</legend>
   
             <label htmlFor="street">Street</label>
-            <input id="street" type="text" />
+            <input id="street" type="text" value={formData.street} onChange={handleChange}/>
   
             <label htmlFor="city">City</label>
-            <input id="city" type="text" />
+            <input id="city" type="text" value={formData.city} onChange={handleChange}/>
   
             <label htmlFor="state">State</label>
-            <select name="state" id="state"></select>
+          <select name="state" id="state" value={formData.state} onChange={handleChange}>
+            <option value="CA">California</option>
+            <option value="NY">New York</option>
+            <option value="TX">Texas</option>
+          </select>
   
             <label htmlFor="zip-code">Zip Code</label>
-            <input id="zip-code" type="number" />
+            <input id="zip-code" type="number" value={formData.zipCode} onChange={handleChange}/>
           </fieldset>
   
           <label htmlFor="department">Department</label>
-          <select name="department" id="department">
+          <select name="department" id="department" value={formData.department} onChange={handleChange}>
             <option value="sales">Sales</option>
             <option value="marketing">Marketing</option>
             <option value="engineering">Engineering</option>
             <option value="hr">Human Resources</option>
             <option value="legal">Legal</option>
           </select>
+
+          <button type="submit">Save Employee</button>
         </form>
-        <button>Save</button>
+       
       </>
     );
   };
